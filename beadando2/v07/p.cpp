a7e00cf4e2917e30ee85033f577afb4d9b1f11f3
Sergei Small <2essergei@gmail.com>
2013-12-12 02:59:25 +0100

    keszul

--- beadando2/v07/p.cpp
+++ beadando2/v07/p.cpp
@@ -9,16 +9,31 @@ using namespace std;
 
 int main(int argc, char* argv[]){
   if(argc != 2){
-    cout << "Hibas argumentum, az argumentum egyetlen ervenyes filenevet kell tartalmaznia."<< endl;
+    cout << "Hibas argumentum, az argumentumnak egyetlen ervenyes filenevet kell tartalmaznia."<< endl;
     exit(0);
   }
-  std::ifstream file(argv[2]);
-  int a;
-  if(file >> a){
-    cout << "Hibas a file tartalma! Kilepes!" << endl;
-    std::exit(0);
+  ifstream file;
+  file.open(argv[1]);
+  if(file.fail()){
+    cout << "A file nem letezik: " << argv[1] << endl << "Kilepes." << endl;
+    exit(0);
   }
-  cout << "OKE: " << a << endl;
+  int a;
+  file >> a;
+  ss_point* A = new ss_point[a];
+  cout << A[0] << endl;
+  /*while(file >> *A){
+    cout << "oks" << endl;
+    //if(file.eof()) break;
+    //cout << "Ertek: " << A << endl;
+  }*/
+  for(int i=0; i<a; i++){
+    file >> A[i];
+  }
+  file.close();
+  //cout << "oks2" << endl;
+  for(int i=0; i<a; i++)
+    cout << "OKE: " << A[i] << endl;
   
   return 0;
 }
